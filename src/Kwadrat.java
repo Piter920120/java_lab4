bcacafe7bfb354250b1555b7a2d0af60c809819b
LENOVO <piotrrrynkowskii@gmail.com>
2022-01-30 08:21:38 +0100

    ćw 3

---
+++ src/Kwadrat.java
@@ -0,0 +1,11 @@
+public class Kwadrat extends  Figura
+{
+    @Override
+    double pole(double a, double b){
+        return a * b;
+    }
+    @Override
+    double obwod(double a, double b){
+        return a + a  +  b + b;
+    }
+}
